988ae6802da9352b1fe4ad2a261c81c2b7e8f118
RsAllies <133039174+RsAllies@users.noreply.github.com>
2023-05-13 09:46:36 +0900

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int stirling(int n, int k) {
+    if (n == 0 && k == 0) {
+        return 1;
+    } else if (n == 0 || k == 0) {
+        return 0;
+    } else {
+        return k * stirling(n-1, k) + stirling(n-1, k-1);
+    }
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k÷
+    cout << "S(" << n << ", " << k << ") = " << stirling(n, k) << endl;
+    
+    return 0;
+}
